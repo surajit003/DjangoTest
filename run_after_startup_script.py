7b93fe3b807c23bcd2d0ca3044307ca6aa421c0e
surajit <surajit@poweredbypeople.io>
2020-10-25 15:42:15 +0300

    minor fix

--- run_after_startup_script.py
+++ run_after_startup_script.py
@@ -1,3 +1,7 @@
+"""
+RUN THIS FILE WHEN THE NEW APP IS CREATED
+"""
+
 import os
 import logging
 
@@ -8,13 +12,11 @@ def run_on_startup():
     os.system("pip install -r requirements.txt --upgrade")
     # importing it here to make sure Django gets installed first
     from django.core.management.utils import get_random_secret_key
-    from django.core.management import execute_from_command_line
 
     os.system(">.env")
     with open(".env", "w+") as f:
         f.write("SECRET_KEY={}".format(get_random_secret_key()))
     os.system("pre-commit install")
-    execute_from_command_line(["manage.py", "migrate"])
 
 
 if __name__ == "__main__":
